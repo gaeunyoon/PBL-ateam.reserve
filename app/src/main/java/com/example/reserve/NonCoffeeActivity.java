6d74d2d1b030f7a41215ff8b4516e55f7852028d
Nokyungmin345 <nkm2008@naver.com>
2020-06-23 21:38:25 +0900

    zzz

--- app/src/main/java/com/example/reserve/NonCoffeeActivity.java
+++ app/src/main/java/com/example/reserve/NonCoffeeActivity.java
@@ -1,7 +1,10 @@
 
 package com.example.reserve;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,16 @@ public class NonCoffeeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nonmenu);
+
+        Button cartbutton = (Button) findViewById(R.id.cartButton);
+        cartbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), OrderActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
